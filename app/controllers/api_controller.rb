e90e79f675219f887f1309e10193fdb594000677
Jake <Jake@MyBandStock.com>
2010-05-10 17:28:00 -0400

    Worked on API - change_stream_permission

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,133 +1,101 @@
 class ApiController < ApplicationController
+  # Required inputs for all API calls:
+  #   api_key
+  #   hash      - sha256(API key + secret key)
+  #   version   - API version number
+  # Optional:
+  #   format    - One of 'xml', 'json', or 'yaml', designating the format of the returned values
+  #               Defaults to JSON
+  #
+  # On success, the API functions returns the unmodified API input variables in the format specified.
+  #
+
+  #before_filter :auth_check
 
   def change_stream_permission
-    if (params[:api_key].nil? || params[:secret_key].nil? || params[:api_version].nil?)
+    # Input: the above input is required; the following are optional:
+    #   email, can_view, can_chat, quality_level
+    # If email is not specified, nothing will happen (but still return success)
+    # If email is specified, the specified privileges will be applied to that user.
+    # On success, the parameters are sent back in the format specified
+    if (auth(params[:api_key], params[:hash], params[:api_version]) == false)
+      raise unauthorized
+=begin
       @output = false
       respond_to do |format|
         format.xml  { render :xml => @output }
         format.json  { render :json => @output }
         # format.yaml { render :yaml => @output }
       end
-      return
+      #return
+=end
     end
 
+    api_version = params[:api_version]
     api_key = params[:api_key]
-    secret_key = params[:secret_key]
-    response_format = params[:format]
-    hash = Digest::MD5.hexdigest(api_key + secret_key) if params[:auto_generate_hash].nil? else params[:hash]\
-    
-    if (response_format.nil?)
-      response_format = 'json'
+    format = params[:format]
+    email = params[:email]
+    hash = params[:hash]
+
+    if (format.nil?)
+      format = 'json'
     end
 
-    @output = { :test1 => 'asdf', :test2 => 'asdf2' }
+    @output = { :api_key => api_key,
+                :hash => hash,
+                :api_version => api_version,
+                :email => email,
+                :can_view => can_view,
+                :can_chat => can_chat }
 #    respond_to do |format|
 #      format.xml { render :xml => @output }
 #      format.json  { render :json => @output }
 #      # format.yaml { render :yaml => @output }
 #    end
-    if (response_format == 'json')
+    if (format == 'json')
       render :json => @output
-    elsif (response_format == 'xml')
+    elsif (format == 'xml')
       render :xml => @output
-    elsif (response_format == 'yaml')
-      return :render @output.to_yaml
+    elsif (format == 'yaml')
+      # Output YAML from here
     end
   end
 
   def test
   end
 
+private
 
+  def auth(api_key, input_hash, api_version)
+    # Takes API POST input as function parameters, and returns false if the request is unauthorized, true otherwise.
 
+    # Retrun false if:
+    #   a parameter is missing
+    #   there is no association in the DB between the given api_key and secret_key
+    #   the hash wasn't right
 
-
-
-
-
-
-
-
-
-  # GET /bands
-  # GET /bands.xml
-  def index
-    @bands = Band.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @bands }
+    if (api_key.nil? || input_hash.nil? || api_version.nil?)
+      return false
     end
-  end
 
-  # GET /bands/1
-  # GET /bands/1.xml
-  def show
-    @band = Band.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @band }
+    # If the api_key does not exist in DB
+    if (! (api_user = ApiUser.find_by_api_key(api_key)) )
+      return false
     end
-  end
-
-  # GET /bands/new
-  # GET /bands/new.xml
-  def new
-    @band = Band.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @band }
+    
+    secret_key = api_user.secret_key
+    test_hash = Digest::SHA2.hexdigest(api_key.to_s + secret_key.to_s)
+    # If given is not what it is supposed to be; this could either be because
+    #  they simply calculated the hash wrong, or did not use the right secret key
+    if (input_hash != test_hash.to_s)
+      return false
     end
+
+    return true
   end
 
-  # GET /bands/1/edit
-  def edit
-    @band = Band.find(params[:id])
-  end
 
-  # POST /bands
-  # POST /bands.xml
-  def create
-    @band = Band.new(params[:band])
 
-    respond_to do |format|
-      if @band.save
-        format.html { redirect_to(@band, :notice => 'Band was successfully created.') }
-        format.xml  { render :xml => @band, :status => :created, :location => @band }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @band.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
 
-  # PUT /bands/1
-  # PUT /bands/1.xml
-  def update
-    @band = Band.find(params[:id])
 
-    respond_to do |format|
-      if @band.update_attributes(params[:band])
-        format.html { redirect_to(@band, :notice => 'Band was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @band.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bands/1
-  # DELETE /bands/1.xml
-  def destroy
-    @band = Band.find(params[:id])
-    @band.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(bands_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -1,4 +1,7 @@
 class ApiUser < ActiveRecord::Base
 	has_many :live_stream_series
 
+  validates_presence_of :api_key, :secret_key
+  validates_uniqueness_of :api_key
+
 end
