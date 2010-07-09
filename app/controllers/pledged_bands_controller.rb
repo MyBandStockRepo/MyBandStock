140ba120a02283c234e594849cfa5dae8dc9bc31
Brian Jennings <brianjennings@macintosh-3.(none)>
2010-07-09 13:10:46 -0400

    stream history and stream crud views nice and pretty

--- app/controllers/pledged_bands_controller.rb
+++ app/controllers/pledged_bands_controller.rb
@@ -3,7 +3,7 @@ class PledgedBandsController < ApplicationController
   # GET /pledged_bands
   # GET /pledged_bands.xml
   def index
-    @pledged_bands = PledgedBand.find(:all, :conditions => ['name LIKE ?', "%#{params[:q]}%"], :order => "name", :include => "pledges")    
+    @pledged_bands = PledgedBand.find(:all, :conditions => ['name LIKE ?', "%#{params[:q]}%"], :order => "pledges_count DESC", :include => "pledges")    
     
     respond_to do |format|
       format.js   { render :layout => false }
--- public/javascripts/access_schedule.js
+++ public/javascripts/access_schedule.js
@@ -1,5 +1,5 @@
 jQuery.noConflict();
-var mbsDomain = 'http://127.0.0.1:3000';
+var mbsDomain = 'http://mybandstock.com';
 var redeemDefaultText = 'Or Enter Your Share Code Here';
 
 
