2de11e2a49cbad17c04bfda2a4aa62db90e79581
Jake Schwartz <Jake@MyBandStock.com>
2010-07-13 14:35:27 -0400

    Updated Band.search_by_name method

--- app/models/band.rb
+++ app/models/band.rb
@@ -54,8 +54,8 @@ class Band < ActiveRecord::Base
     # 'The Daft Punk'
     
     query_string =
-        ["name IN (?, ?, ?, ?) OR short_name IN (?, ?, ?, ?)"
-          name, name.downcase, 'The ' + name, name.gsub('The ', '')
+        ["name IN (?, ?, ?, ?) OR short_name IN (?, ?, ?, ?)",
+          name, name.downcase, 'The ' + name, name.gsub('The ', ''),
           name.gsub(' ', '').downcase, name.gsub(' ', '_').downcase, name.gsub(' ', '').upcase, name.downcase.gsub('the ', '')
         ]
     logger.info Band.where(query_string).to_sql
